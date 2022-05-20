7abe579d9c1937e3a5f6d89f5ffcc2eb6c9c77ed
yatsev1ch <ayatsevdeveloper@gmail.com>
2022-05-21 02:37:38 +0300

    Добавлены анимации слайдера

--- source/js/slider.js
+++ source/js/slider.js
@@ -14,44 +14,60 @@ let reviewsTouchStartX;
 let trainersTouchStartX;
 let trainerTarget;
 
-const slideLeft = (elementsGetter, elementsContainer) => {
-  const elements = elementsGetter();
+const shiftElementAtStart = (elementsGetter, elementsContainer, alignment) => {
+  let elements = elementsGetter();
   const lastElement = elements[elements.length - 1];
   const lastElementClone = lastElement.cloneNode(true);
   const firstElement = elements[0];
   lastElement.remove();
   elementsContainer.insertBefore(lastElementClone, firstElement);
+  elements = elementsGetter();
+  elements[1].scrollIntoView({behavior: 'instant', block: 'nearest', inline: alignment});
 };
 
-const slideRight = (elementsGetter, elementsContainer) => {
-  const elements = elementsGetter();
+shiftElementAtStart(getCurrentTrainers, trainersContainer, 'start');
+shiftElementAtStart(getCurrentReviews, reviewsContainer, 'center');
+
+const slideLeft = (elementsGetter, elementsContainer, alignment) => {
+  let elements = elementsGetter();
+  const lastElement = elements[elements.length - 1];
+  const lastElementClone = lastElement.cloneNode(true);
+  const firstElement = elements[0];
+  lastElement.remove();
+  elementsContainer.insertBefore(lastElementClone, firstElement);
+  elements = elementsGetter();
+  elements[2].scrollIntoView({behavior: 'instant', block: 'nearest', inline: alignment});
+  elements[1].scrollIntoView({behavior: 'smooth', block: 'nearest', inline: alignment});
+};
+
+const slideRight = (elementsGetter, elementsContainer, alignment) => {
+  let elements = elementsGetter();
   const firstElement = elements[0];
   const firstElementClone = elements[0].cloneNode(true);
   firstElement.remove();
   elementsContainer.appendChild(firstElementClone);
+  elements = elementsGetter();
+  elements[0].scrollIntoView({behavior: 'instant', block: 'nearest', inline: alignment});
+  elements[1].scrollIntoView({behavior: 'smooth', block: 'nearest', inline: alignment});
 };
 
-const handleTouchSwipe = (touchStart, touchEnd, elementsGetter, elementsContainer) => {
-  if (Math.abs(touchStart - touchEnd) < 20) {
-    return;
-  }
-
+const handleTouchSwipe = (touchStart, touchEnd, elementsGetter, elementsContainer, alignment) => {
   if (touchStart > touchEnd) {
-    slideRight(elementsGetter, elementsContainer);
+    slideRight(elementsGetter, elementsContainer, alignment);
   }
 
   if (touchStart < touchEnd) {
-    slideLeft(elementsGetter, elementsContainer);
+    slideLeft(elementsGetter, elementsContainer, alignment);
   }
 };
 
 
 reviewsPrevButton.addEventListener('click', ()=> {
-  slideLeft(getCurrentReviews, reviewsContainer);
+  slideLeft(getCurrentReviews, reviewsContainer, 'center');
 });
 
 reviewsNextButton.addEventListener('click', ()=> {
-  slideRight(getCurrentReviews, reviewsContainer);
+  slideRight(getCurrentReviews, reviewsContainer, 'center');
 });
 
 reviewsContainer.addEventListener('touchstart', (evt) => {
@@ -62,22 +78,23 @@ reviewsContainer.addEventListener('touchstart', (evt) => {
 
 reviewsContainer.addEventListener('touchend', (evt) => {
   const touchEndX = evt.changedTouches[0].screenX;
-  handleTouchSwipe(reviewsTouchStartX, touchEndX, getCurrentReviews, reviewsContainer);
+  handleTouchSwipe(reviewsTouchStartX, touchEndX, getCurrentReviews, reviewsContainer, 'center');
   page.classList.remove('page--without-scroll');
 });
 
 trainersPrevButton.addEventListener('click', ()=> {
-  slideLeft(getCurrentTrainers, trainersContainer);
+  slideLeft(getCurrentTrainers, trainersContainer, 'start');
 });
 
 trainersNextButton.addEventListener('click', ()=> {
-  slideRight(getCurrentTrainers, trainersContainer);
+  slideRight(getCurrentTrainers, trainersContainer, 'start');
 
 });
 
 trainersContainer.addEventListener('touchstart', (evt) => {
   const trainer = evt.target.closest('.trainers__trainer');
   if (trainer) {
+    evt.preventDefault();
     trainerTarget = trainer;
     trainerTarget.classList.add('trainers__trainer--on-swiping');
     page.classList.add('page--without-scroll');
@@ -85,18 +102,15 @@ trainersContainer.addEventListener('touchstart', (evt) => {
   }
 });
 
-trainersContainer.addEventListener('touchcancel', () => {
-  trainerTarget.classList.remove('trainers__trainer--on-swiping');
-  trainerTarget = null;
-  page.classList.remove('page--without-scroll');
-});
-
 trainersContainer.addEventListener('touchend', (evt) => {
   const touchEndX = evt.changedTouches[0].screenX;
-  handleTouchSwipe(trainersTouchStartX, touchEndX, getCurrentTrainers, trainersContainer);
-  if (trainerTarget) {
-    trainerTarget.classList.remove('trainers__trainer--on-swiping');
-    trainerTarget = null;
-    page.classList.remove('page--without-scroll');
+  if (Math.abs(trainersTouchStartX - touchEndX) < 2) {
+    trainerTarget.focus();
+  } else {
+    handleTouchSwipe(trainersTouchStartX, touchEndX, getCurrentTrainers, trainersContainer, 'start');
   }
+  trainerTarget.classList.remove('trainers__trainer--on-swiping');
+  trainerTarget = null;
+  trainersTouchStartX = null;
+  page.classList.remove('page--without-scroll');
 });
