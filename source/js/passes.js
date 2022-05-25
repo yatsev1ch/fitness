6023e808b824ea6771bb7362ec748d889e4da35c
yatsev1ch <ayatsevdeveloper@gmail.com>
2022-05-25 18:06:48 +0300

    Исправлены ошибки 3

--- source/js/passes.js
+++ source/js/passes.js
@@ -1,25 +1,7 @@
-const PASSES_PRICES = {
-  'month': {
-    'trainer': 5000,
-    'day': 1700,
-    'fullDay': 2700,
-  },
-  'halfYear': {
-    'trainer': 15000,
-    'day': 9900,
-    'fullDay': 12300,
-  },
-  'year': {
-    'trainer': 25000,
-    'day': 19000,
-    'fullDay': 596000,
-  },
-};
-
 const togglesContainer = document.querySelector('.passes__toggles');
 const passesContainer = document.querySelector('.passes__passes-list');
 const toggles = togglesContainer.querySelectorAll('.passes__toggle');
-const priceElements = passesContainer.querySelectorAll('[data-type]');
+const priceElements = passesContainer.querySelectorAll('.passes__pass');
 
 const changeToggleState = (selectedToggle) => {
   toggles.forEach((toggle) => {
@@ -33,11 +15,12 @@ const changeToggleState = (selectedToggle) => {
 
 const changePriceWith = (toggle) => {
   const toggleType = toggle.dataset.type;
-  const pricesObject = PASSES_PRICES[`${toggleType}`];
   priceElements.forEach((priceElement) => {
-    const type = priceElement.dataset.type;
-    priceElement.dataset.price = pricesObject[`${type}`];
-    priceElement.innerHTML = `${pricesObject[`${type }`]}&nbsp;`;
+    if (priceElement.classList.contains(`passes__pass--${toggleType}`)) {
+      priceElement.classList.remove('passes__pass--hidden');
+    } else {
+      priceElement.classList.add('passes__pass--hidden');
+    }
   });
 };
 
